43d97bf3fec159893f679e90636259abf2fc91c9
nathyalves <132521294+nathyalves@users.noreply.github.com>
2024-02-18 01:36:25 -0300

    Enviando arquivos

---
+++ assets/js/script.js
@@ -0,0 +1,45 @@
+const watchDocument = document.querySelector("#watch");
+let seconds = 0;
+let minutes = 0;
+let hours = 0;
+let interval;
+
+function init() {
+    watch();
+    interval = setInterval(watch, 1000);
+}
+
+const digitZero = (digit) => {
+    if (digit < 10) {
+        return `0${digit}`;
+    } else {
+        return digit;
+    }
+}
+
+const pause = () => {
+    clearInterval(interval);
+}
+
+const clearWatch = () => {
+    clearInterval(interval);
+    seconds = 0;
+    minutes = 0;
+    hours = 0;
+    watchDocument.innerHTML = "00:00:00";
+}
+
+function watch() {
+     seconds++;
+
+    if (seconds === 60) {
+        minutes++;
+        seconds = 0;
+    }
+    if (minutes === 60) {
+        minutes = 0;
+        hours++;
+    }
+            
+     watchDocument.innerHTML = digitZero(hours) + ":" + digitZero(minutes) + ":" + digitZero(seconds);
+}
